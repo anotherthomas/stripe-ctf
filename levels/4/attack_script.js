475fa5b839fd0d9d2b57df6b179b674126cbd112
tom <thomas.fenzl@gmx.net>
2013-11-05 21:36:49 +0100

    updated sqlite for newer nodejs

---
+++ levels/4/attack_script.js
@@ -0,0 +1,1 @@
+<script>document.getElementsByName('to')[0].value = 'thief'; document.getElementsByName('amount')[0].value = 1; document.getElementsByTagName('Input')[2].click()</script>
---
+++ levels/portal/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+location='http://localhost'
+baseport=8000
+
+
+def define_environments(baseport, number):
+    return [{'port': baseport+x*50, 'number': x} for x in xrange(number)]
+
+@app.route('/')
+def index():
+    return render_template('index.html', location=location, environments=define_environments(8000, 10))
+
+
+@app.route('/<int:instance>')
+def levels(instance):
+    return render_template('levels.html', location=location, baseport=baseport+(instance*50), user_id=instance)
+
+@app.route('/<int:instance>/serverconfig')
+def config(instance):
+    return render_template('instance.conf', instance=instance, baseport=baseport)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=1025)
